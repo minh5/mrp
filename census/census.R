6975f0b93cf99f999c986593f9b6a94e473d36b2
minh <minh@0ptimus.com>
2015-10-07 01:16:47 -0400

    wrote code for census poststratification

---
+++ census/census.R
@@ -0,0 +1,21 @@
+#loading up individual level census daa
+setwd('~//mrp'
+census <- rbind(data.table::fread('census/ss13pusa.csv', select = c('ST','AGEP','RAC1P','SCHL','HISP'), data.table = FALSE),
+                data.table::fread('census/ss13pusb.csv', select = c('ST','AGEP','RAC1P','SCHL','HISP'), data.table = FALSE))
+                
+#refactoring variables to match modeling methods
+census$age <- cut(census$AGEP, breaks = c(-Inf,18,35,55,Inf), labels = c("under 18","18-35","35-55", "55+"))
+census$education <- cut(census$SCHL, breaks = c(0,16, 21, Inf), labels = c('Up to HS', 'Up to College', 'Postgraduate'))
+census$race <- factor(census$RAC1P)
+levels(census$race) <- c('White', 'Black', 'Native', 'Native', 'Native', 'Asian', 'Asian', 'Other', 'Other')
+census$hisp <- ifelse(census$HISP == 1, "Hisp", "Not Hisp")
+
+#creating cohort levels to get proportions
+census$cohort <- paste(census$ST, census$age, census$education, census$race, census$hisp, sep=',')
+
+#custom function to grab proportions of each cohort by state
+porpFunct <- function(x) {prop.table(table(x))}
+proportions <- tapply(census$cohort, census$ST, porpFunct) 
+proportions <- do.call(rbind, lapply(test, data.frame))
+
+write.csv(proportions, 'census/post-strat.csv', row.names=F)
