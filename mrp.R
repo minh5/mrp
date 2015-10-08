892fecb24c84b9663943df4aad84a4e8f9d0e7ce
minh5 <minh.v.mai@gmail.com>
2015-10-08 17:35:20 -0400

    quickly wrote calculations for Montana

--- mrp.R
+++ mrp.R
@@ -4,7 +4,7 @@ library(caret)
 
 load("data.RData")
 
-#break up ages, education, and political ideology
+#break up ages and education
 ageGroups <- as.data.frame(cut(tmpData$demAgeFull, breaks = c(0,18,35,55,Inf), labels = c("under 18","18-35","35-55", "55+")))
 names(ageGroups) <- "ageGroups"
 eduGroups <- as.data.frame(cut(tmpData$demEduFull, breaks = c(0,3,7,9), labels = c("Up to HS", "Up to College", "PostGraduate")))
@@ -38,7 +38,6 @@ data$southeast <- ifelse(data$demState %in% c(1,18, 25, 43), 1, 0)
 data$southwest <- ifelse(data$demState %in% c(2, 19, 37, 44), 1, 0)
 data$moutain <-  ifelse(data$demState %in% c(3, 6, 13, 32, 27,45, 51), 1, 0)
 data$pacific <- ifelse(data$demState %in% c(2,5, 12, 38, 48), 1, 0)
-names(data)
 data <- data[,-grep("^demState",names(data))]
 
 #creating columns where we are trying to measure predict
@@ -97,22 +96,6 @@ cart_tune = train(right ~ . ,
 cart_tune
 results$cart <- predict(cart_tune, newdata = test, type = 'prob')
 
-#Boosting because I heard it's really good machine learning algorithm (commented because it takes too long to iterate)
-# require(gbm)
-# tuningGrid <- expand.grid(.n.trees = seq(200,1000,by = 200), 
-#                           .interaction.depth = seq(1,7, by = 3 ),
-#                           .shrinkage = seq(.001, .01, by = .001),
-#                           .n.minobsinnode = seq(20,60, by = 20))
-# 
-# gbm_tune = train(right ~ . , 
-#                  data = train ,
-#                  method = "gbm" ,
-#                  metric = "ROC" ,
-#                  tuneGrid = tuningGrid,
-#                  trControl = control)
-# gbm_tune
-# results$gbm <- predict(gbm_tune, newdata = test)
-
 #Alot of talk has been on the accuracy of neural network algorithm, having a go at it here.
 require(nnet)
 tuningGrid<-expand.grid(.size = 1:5, .decay = c(0, 2, by = .5))
@@ -151,13 +134,17 @@ post <- cbind(data, dataPrep)
 post$race <- factor(post$demRace)
 levels(post$race) <- c('Native', 'Asian', 'Black', 'White', 'Other')
 post$gender <- ifelse(post$demGender == 1, "Male", "Female")
-post$hisp <-ifelse(post$demHisp == 1, "Hisp", "Not Hisp")                      
-post$state <- factor(post$demState)
-levels(post$state) <- c(1,2,4,5,6,8,9,10,11,12,13,15,
-                        16,17,18,19,20,21,22,23,24,25,
-                        26,27,28,29,30,31,32,33,24,25,
-                        36,37,38,39,40,41,42,44,45,46,
-                        47,48,49,50,51,53,54,55,56,72)
+post$hisp <-ifelse(post$demHisp == 1, "Hisp", "Not Hisp") 
+post <- cbind(post, tmpData$demState)
+names(post)[ncol(post)] <- 'demState'
+stateLookup <- data.frame(demState = seq(1,52), state = c(1,2,4,5,6,8,9,10,11,12,13,15,
+                                                      16,17,18,19,20,21,22,23,24,25,
+                                                      26,27,28,29,30,31,32,33,24,25,
+                                                      36,37,38,39,40,41,42,44,45,46,
+                                                      47,48,49,50,51,53,54,55,56,72))
+
+post <- merge(post, stateLookup, by = "demState", all.x = T)
+
 post$cohort <- paste(post$state, post$gender, post$ageGroups, post$eduGroups, post$race, post$hisp, sep=',')
 census <- read.csv('census/post-strat.csv')                      
 
@@ -174,4 +161,27 @@ post$weights[which(is.na(post$weights))] <- match[which(is.na(match$weights)), '
 #post-stratification calcuations
 post$finalScore <- post$prediction[[1]] * post$weights
 tapply(post$finalScore, post$state, function(x) sum(x)*100)
+
+#notice that Montana is missing
+#to estimate the score, I'm going to grab the average sample population makeup of ID, WY, ND, and SD because they neighbor Montana. I have no reason to think that their population
+#make is significantly different from Montana's
+
+#grabbing from data set with the original tmpData frame as the index
+mountain <- data[tmpData$demState %in% c(13, 35, 42, 51), ]
+
+#loop to grab the averages of the dataset and append it onto Montana's
+montana <- data.frame()
+for (i in 2:26){
+  montana <- rbind(montana, mean(mountain[,i]))
+}
+
+#reformatting dataset to match the training and test sets
+montana <- as.data.frame(t(montana))
+names(montana) <- names(train)[2:26]
+row.names(montana) <- "montana"
+
+#applying prediction models
+montana$predictions <- predict(nnet_tune, newdata = montana, type = 'prob')[2]
+montana$finalScore <- 
+append(tapply(post$finalScore, post$state, sum), montana$predictions)
 
