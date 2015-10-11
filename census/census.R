b949bc9a0ba44b886628b274bdbfcf9be0ca737e
minh <minh@0ptimus.com>
2015-10-11 00:58:31 -0400

    reorganized code to match mrp methodology

--- census/census.R
+++ census/census.R
@@ -1,13 +1,13 @@
 #loading up individual level census daa
 setwd('~/mrp/census/')
-census <- rbind(data.table::fread('census/ss13pusa.csv', select = c('ST','SEX','AGEP','RAC1P','SCHL','HISP'), data.table = FALSE),
-                data.table::fread('census/ss13pusb.csv', select = c('ST','SEX', 'AGEP','RAC1P','SCHL','HISP'), data.table = FALSE))
+census <- rbind(data.table::fread('census/ss13pusa.csv', select = c('ST','SEX','AGEP','RAC1P','SCHL','HISP','PWGTP'), data.table = FALSE),
+                data.table::fread('census/ss13pusb.csv', select = c('ST','SEX', 'AGEP','RAC1P','SCHL','HISP','PWGTP'), data.table = FALSE))
                 
 #refactoring variables to match modeling methods
 census$gender <- ifelse(census$SEX == 1, "Male", "Female")
 census$age <- cut(census$AGEP, breaks = c(-Inf,18,35,55,Inf), labels = c("under 18","18-35","35-55", "55+"))
 census$SCHL[which(is.na(census$SCHL))] <- 0
-census$education <- cut(census$SCHL, breaks = c(0,16, 21, Inf), labels = c('Up to HS', 'Up to College', 'PostGraduate'))
+census$education <- cut(census$SCHL, breaks = c(-Inf,16, 21, Inf), labels = c('Up to HS', 'Up to College', 'PostGraduate'))
 census$race <- factor(census$RAC1P)
 levels(census$race) <- c('White', 'Black', 'Native', 'Native', 'Native', 'Asian', 'Asian', 'Other', 'Other')
 census$hisp <- ifelse(census$HISP == 1, "Hisp", "Not Hisp")
@@ -18,19 +18,12 @@ census2 <- as.data.frame(predict(dummiesCensus, newdata = census))
 names(census2) <- c("female","male","under18","age18.35","age35.55","age55plus","hs","college",
                     "postgraduate","white","black","native","asian","other.race","hisp","notHisp")
 
-stateLookup <- data.frame(state = c(1,2,4,5,6,8,9,10,11,12,13,15,
-                                    16,17,18,19,20,21,22,23,24,25,
-                                    26,27,28,29,30,31,32,33,24,25,
-                                    36,37,38,39,40,41,42,44,45,46,
-                                    47,48,49,50,51,53,54,55,56,72), demState = seq(1,52))
+
 census2 <- cbind(census2, census$ST)
 names(census2)[ncol(census2)] <- 'ST'
 census2$demState <- factor(census2$ST)
-levels(census2$demState) <- c(1,2,4,5,6,8,9,10,11,12,13,15,
-                             16,17,18,19,20,21,22,23,24,25,
-                             26,27,28,29,30,31,32,33,24,25,
-                             36,37,38,39,40,41,42,44,45,46,
-                             47,48,49,50,51,53,54,55,56,72)
+table(census2$demState)
+levels(census2$demState) <- seq(1,51)
 
 #dummying out regional again
 census2$newengland <- ifelse(census2$demState %in% c(7, 20, 22, 30, 40, 46), 1,0)
@@ -45,21 +38,22 @@ census2$pacific <- ifelse(census2$demState %in% c(2,5, 12, 38, 48), 1, 0)
 
 #applying the algorithm
 census2$predictionRight <- predict(nnet_tune1, newdata = census2, type = 'prob')[2]
-names(census2)
-names(census2)
-names(train)
+census2$predictionApprove <- predict(nnet_tune2, newdata = census2, type = 'prob')[2]
+census2$predictionEcon <- predict(nnet_tune3, newdata = census2, type = 'prob')[2]
 
-str(train)
-str(census2)
 
-#creating cohort levels to get proportions
-census$cohort <- paste(census$ST, census$gender, census$age, census$education, census$race, census$hisp, sep=',')
-head(census$cohort)
-#custom function to grab proportions of each cohort by state
-options(scipen=9999)
-porpFunct <- function(x) {prop.table(table(x))}
-proportions <- tapply(census$cohort, census$ST, function(x) prop.table(table(x))) 
-proportions <- do.call(rbind, lapply(proportions, data.frame))
-names(proportions) <- c("cohort","weights")
+#applying census weights and calculating final scores
+census2 <- cbind(census2, census$PWGTP)
+names(census2)[ncol(census2)] <- 'weights'
 
-write.csv(proportions, 'census/post-strat.csv', row.names=F)
+census2$rightScore <- census2$predictionRight * census2$weights 
+census2$approveScore <- census2$predictionApprove * census2$weights 
+census2$econScore <- census2$predictionEcon * census2$weights 
+
+rScore <- as.data.frame(tapply(as.numeric(unlist(census2$rightScore)),census2$ST,function(x) sum(x)/length(x)))
+aScore <- as.data.frame(tapply(as.numeric(unlist(census2$approveScore)),census2$ST,function(x) sum(x)/length(x)))
+eScore <- as.data.frame(tapply(as.numeric(unlist(census2$econScore)),census2$ST,function(x) sum(x)/length(x)))
+final <- cbind(as.data.frame(levels(as.factor(census2$ST))),rScore,aScore,eScore)
+names(final) <- c("ST","right","approve","econ")
+
+write.csv(final, "predictions.csv",row.names=F)
--- mrp.R
+++ mrp.R
@@ -296,74 +296,64 @@ mean(brierscore(obs ~ nnet, data = eval)) #winner
 predictdf$predictionEconomy <- predict(nnet_tune3, newdata = predictdf, type = 'prob')[2]
 
 #####post-stratification
-#grabbing cohorts from census file for post stratification
-post <- cbind(data, dataPrep)
-post$race <- factor(post$demRace)
-levels(post$race) <- c('Native', 'Asian', 'Black', 'White', 'Other')
-post$gender <- ifelse(post$demGender == 1, "Male", "Female")
-post$hisp <-ifelse(post$demHisp == 1, "Hisp", "Not Hisp") 
-post <- cbind(post, tmpData$demState)
-names(post)[ncol(post)] <- 'demState'
-stateLookup <- data.frame(demState = seq(1,52), state = c(1,2,4,5,6,8,9,10,11,12,13,15,
-                                                      16,17,18,19,20,21,22,23,24,25,
-                                                      26,27,28,29,30,31,32,33,24,25,
-                                                      36,37,38,39,40,41,42,44,45,46,
-                                                      47,48,49,50,51,53,54,55,56,72))
+#combining individual level census data from https://www.kaggle.com/c/2013-american-community-survey/data
+census <- rbind(data.table::fread('census/ss13pusa.csv', select = c('ST','SEX','AGEP','RAC1P','SCHL','HISP','PWGTP'), data.table = FALSE),
+                data.table::fread('census/ss13pusb.csv', select = c('ST','SEX', 'AGEP','RAC1P','SCHL','HISP','PWGTP'), data.table = FALSE))
 
-post <- merge(post, stateLookup, by = "demState", all.x = T)
+#refactoring variables to match modeling methods
+census$gender <- ifelse(census$SEX == 1, "Male", "Female")
+census$age <- cut(census$AGEP, breaks = c(-Inf,18,35,55,Inf), labels = c("under 18","18-35","35-55", "55+"))
+census$SCHL[which(is.na(census$SCHL))] <- 0
+census$education <- cut(census$SCHL, breaks = c(-Inf,16, 21, Inf), labels = c('Up to HS', 'Up to College', 'PostGraduate'))
+census$race <- factor(census$RAC1P)
+levels(census$race) <- c('White', 'Black', 'Native', 'Native', 'Native', 'Asian', 'Asian', 'Other', 'Other')
+census$hisp <- ifelse(census$HISP == 1, "Hisp", "Not Hisp")
 
-post$cohort <- paste(post$state, post$gender, post$ageGroups, post$eduGroups, post$race, post$hisp, sep=',')
-census <- read.csv('census/post-strat.csv')                      
+#dummying census voter file
+dummiesCensus <- dummyVars(ST ~ . , data= census[,c('ST','gender','age','education','race','hisp')])
+census2 <- as.data.frame(predict(dummiesCensus, newdata = census))
+names(census2) <- c("female","male","under18","age18.35","age35.55","age55plus","hs","college",
+                    "postgraduate","white","black","native","asian","other.race","hisp","notHisp")
 
-#post-stratification
-post <- merge(post, census, by = 'cohort', all.x = T)
-table(is.na(post$weights)) #noticed some weights are missing because the cohorts are not represented
+#matching the census state codes to the demState codes
+census2 <- cbind(census2, census$ST)
+names(census2)[ncol(census2)] <- 'ST'
+census2$demState <- factor(census2$ST)
+table(census2$demState)
+levels(census2$demState) <- seq(1,51)
 
-# created a table of average weights to impute into missing values
-means <- as.data.frame(tapply(post$weights, post$state, function(x) mean(x, na.rm = T)))
-names(means) <- "means"
-match <- merge(post, means, by.x = 'state', by.y = 0)
-post$weights[which(is.na(post$weights))] <- match[which(is.na(match$weights)), 'means']
+#dummying out regional again
+census2$newengland <- ifelse(census2$demState %in% c(7, 20, 22, 30, 40, 46), 1,0)
+census2$midatlantic <- ifelse(census2$demState %in% c(31, 33, 39),1,0)
+census2$eastcentral <- ifelse(census2$demState %in% c(16, 14, 23, 36, 50),1,0)
+census2$westcentral <- ifelse(census2$demState %in% c(16, 17, 24, 26, 28, 35, 42),1,0)
+census2$southatlantic <- ifelse(census2$demState %in% c(8, 9, 10, 11, 21, 34, 41, 47, 49),1,0)
+census2$southeast <- ifelse(census2$demState %in% c(1,18, 25, 43), 1, 0)
+census2$southwest <- ifelse(census2$demState %in% c(2, 19, 37, 44), 1, 0)
+census2$moutain <-  ifelse(census2$demState %in% c(3, 6, 13, 32, 27,45, 51), 1, 0)
+census2$pacific <- ifelse(census2$demState %in% c(2,5, 12, 38, 48), 1, 0)
 
-final <- cbind(predictdf[,c("predictionRight","predictionApprove","predictionEconomy")], post[,c("state","weights")])
-#post-stratification calcuations
-final$rightScore <- final$predictionRight[[1]] * final$weights
-final$approveScore <- final$predictionApprove[[1]]*final$weights
-final$econScore <- final$predictionEconomy[[1]]*final$weights
+#applying the algorithm
+census2$predictionRight <- predict(nnet_tune1, newdata = census2, type = 'prob')[2]
+census2$predictionApprove <- predict(nnet_tune2, newdata = census2, type = 'prob')[2]
+census2$predictionEcon <- predict(nnet_tune3, newdata = census2, type = 'prob')[2]
 
-#notice that Montana is missing
-#to estimate the score, I'm going to grab the average sample population makeup of ID, WY, ND, and SD because they neighbor Montana. I have no reason to think that their population
-#make is significantly different from Montana's
-#grabbing from data set with the original tmpData frame as the index
-mountain <- data[tmpData$demState %in% c(13, 35, 42, 51), ]
 
-#loop to grab the averages of the dataset and append it onto Montana's
-montana <- data.frame()
-for (i in 2:26){
-  montana <- rbind(montana, mean(mountain[,i]))
-}
+#applying census weights  to scores
+census2 <- cbind(census2, census$PWGTP)
+names(census2)[ncol(census2)] <- 'weights'
 
-#reformatting dataset to match the training and test sets
-montana <- as.data.frame(t(montana))
-names(montana) <- names(train)[2:26]
-row.names(montana) <- "montana"
+census2$rightScore <- census2$predictionRight * census2$weights 
+census2$approveScore <- census2$predictionApprove * census2$weights 
+census2$econScore <- census2$predictionEcon * census2$weights 
 
-#applying prediction models
-montana$predictionRight <- predict(nnet_tune1, newdata = montana, type = 'prob')[2]
-montana$predictionApprove <- predict(nnet_tune2, newdata = montana, type = 'prob')[2]
-montana$predictionEconomy <- predict(nnet_tune3, newdata = montana, type = 'prob')[2]
-census$state <- substring(census$cohort, 1,2)
-censusMT <- census[census$state == 30,]
+#tabulating scores by state
+rScore <- as.data.frame(tapply(as.numeric(unlist(census2$rightScore)),census2$ST,function(x) sum(x)/length(x)))
+aScore <- as.data.frame(tapply(as.numeric(unlist(census2$approveScore)),census2$ST,function(x) sum(x)/length(x)))
+eScore <- as.data.frame(tapply(as.numeric(unlist(census2$econScore)),census2$ST,function(x) sum(x)/length(x)))
+final <- cbind(as.data.frame(levels(as.factor(census2$ST))),rScore,aScore,eScore)
+names(final) <- c("ST","right","approve","econ")
 
-montana$state <- 30
-montana$weights <- mean(census$weights[census$state == 30])
-montana$rightScore <- as.numeric(montana$predictionRight * montana$weights)
-montana$approveScore <- as.numeric(montana$predictionRight * montana$weights)
-montana$econScore <- as.numeric(montana$predictionRight * montana$weights)
-montana <- montana[,names(final)]
-final <- rbind(final, montana)
 
-#tabulate state scores
-tapply(as.numeric(final$rightScore), final$state, function(x) sum(x)*100)
-tapply(as.numeric(final$approveScore), final$state, function(x) sum(x)*100)
-tapply(as.numeric(final$econScore), final$state, function(x) sum(x)*100)
+#writing results to csv to create maps
+write.csv(final, "predictions.csv",row.names=F)
